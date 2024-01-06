06ac6f1dd758bf115efba4b264d463d9e7a1b078
Jason_Teng <sdonline13@gmail.com>
2024-01-06 21:27:08 +0800

    firest commit

---
+++ src/main/java/com/jason/linenotifyproject/LineNotifyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jason.linenotifyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineNotifyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LineNotifyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jason/linenotifyproject/LineNotifyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.linenotifyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineNotifyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
